5fbcb12510fc7e346c8ac97bd39331b6eea2f20c
salazharS <felipelula@hotmail.com.br>
2026-03-02 19:02:43 -0300

    atualizado variaveis

--- bkp.ps1
+++ bkp.ps1
@@ -1,12 +1,14 @@
 $recovery = "R:"
-$date = Get-Date -Format "yyyy-MM-dd_HH-mm"
-$local_zip = "R:\WindowsImageBackup_$date.zip"
+$bkp_disk = "C:"
+$hd_name = "SAMSUNG HD502HI"
 
-$currentBackup = "R:\WindowsImageBackup"
-$oldBackup     = "R:\WindowsImageBackup_OLD"
+$date = Get-Date -Format "yyyy-MM-dd_HH-mm"
+$local_zip = "$recovery\WindowsImageBackup_$date.zip"
+$bkp_atual = "$recovery\WindowsImageBackup"
+$old_bkp     = "$recovery\WindowsImageBackup_OLD"
 
 # ===== VERIFICA SAÚDE DO HD =====
-$disk = Get-PhysicalDisk | Where-Object FriendlyName -eq "SAMSUNG HD502HI"
+$disk = Get-PhysicalDisk | Where-Object FriendlyName -eq $hd_name
 
 if ($null -eq $disk) {
     Write-Host "Disco não encontrado."
@@ -16,33 +18,33 @@ elseif ($disk.HealthStatus -eq "Healthy") {
     Write-Host "HD saudável. Iniciando processo..."
 
     # ===== SE EXISTIR BACKUP ATUAL, RENOMEIA PARA _OLD =====
-    if (Test-Path $currentBackup) {
+    if (Test-Path $bkp_atual) {
 
         # Se já existir um OLD antigo, remove
-        if (Test-Path $oldBackup) {
-            Remove-Item $oldBackup -Recurse -Force
+        if (Test-Path $old_bkp) {
+            Remove-Item $old_bkp -Recurse -Force
         }
 
-        Rename-Item $currentBackup $oldBackup
+        Rename-Item $bkp_atual $old_bkp
         Write-Host "Backup anterior renomeado para _OLD."
     }
 
     # ===== EXECUTA BACKUP =====
-    wbadmin start backup -backupTarget:$recovery -include:C: -allCritical -quiet
+    wbadmin start backup -backupTarget:$recovery -include:$bkp_disk -allCritical -quiet
 
     if ($LASTEXITCODE -eq 0) {
 
         Write-Host "Backup concluído com sucesso."
 
         # ===== REMOVE O OLD =====
-        if (Test-Path $oldBackup) {
-            Remove-Item $oldBackup -Recurse -Force
+        if (Test-Path $old_bkp) {
+            Remove-Item $old_bkp -Recurse -Force
             Write-Host "Backup antigo removido."
         }
 
         # ===== COMPACTA O NOVO =====
         Write-Host "Compactando novo backup..."
-        tar -a -c -f $local_zip -C "R:\" "WindowsImageBackup"
+        tar -a -c -f $local_zip -C "$recovery\" "WindowsImageBackup"
 
         Write-Host "Processo concluído com sucesso."
     }
@@ -50,8 +52,8 @@ elseif ($disk.HealthStatus -eq "Healthy") {
         Write-Host "Erro no wbadmin!"
 
         # ===== RESTAURA O OLD SE DER ERRO =====
-        if (Test-Path $oldBackup) {
-            Rename-Item $oldBackup $currentBackup
+        if (Test-Path $old_bkp) {
+            Rename-Item $old_bkp $bkp_atual
             Write-Host "Backup anterior restaurado."
         }
     }
